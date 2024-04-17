8f459655c66fa5b9fa4db7c2feea6b9eac70b214
Abinash4567 <abinashyadav4567@gmail.com>
2024-04-18 02:35:58 +0530

    feature hero sectino

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,7 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
 import "./globals.css";
-
-const inter = Inter({ subsets: ["latin"] });
+import { ThemeProvider } from "@/components/theme-provider"
+import { playfair } from "@/font/font";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -16,7 +15,16 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={playfair.className}>
+        <ThemeProvider
+              attribute="class"
+              defaultTheme="system"
+              enableSystem
+              disableTransitionOnChange
+            >
+        {children}
+        </ThemeProvider>
+      </body>
     </html>
   );
 }
